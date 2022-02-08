7fcac0ce908fabde8e5882e2f19c79e11ce47628
kiwi <git@kirkkirk.co.uk>
2022-02-08 10:53:23 +0000

    Updated settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-amy7attdmenpsiiuhv1r(h7h-4kps*j*_nwyl89e#glyyaay#4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
